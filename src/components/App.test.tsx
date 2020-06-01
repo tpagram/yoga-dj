a0e0b46ae3f50280f787c3561388006da4f15d57
Thomas Pagram <tpagram@gmail.com>
2020-06-01 18:16:52 +1000

    Added templates for the remaining integration tests

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -10,3 +10,48 @@ test('fetches and displays a list of scenes in the default workout', async () =>
   expect(queryByText(/timer scene 1/i)).toBeTruthy();
   expect(queryByText(/timer scene 2/i)).toBeTruthy();
 })
+
+// TODO
+test('when another workout is selected, it loads that workout scenes', async () => {
+  expect(true).toBeTruthy();
+})
+
+// TODO
+test('when the start button is pressed, it starts playing the workout', async () => {
+  expect(true).toBeTruthy();
+})
+
+// TODO
+test('when a workout contains a timer scene, it should show a timer', async () => {
+  expect(true).toBeTruthy();
+})
+
+// TODO
+test('when a workout contains a rest scene, it should show a timer', async () => {
+  expect(true).toBeTruthy();
+})
+
+// TODO
+test('when a workout contains a video scene, it should show a video player', async () => {
+  expect(true).toBeTruthy();
+})
+
+// TODO
+test('when a timer is showing, the pause button should pause the timer', async () => {
+  expect(true).toBeTruthy();
+})
+
+// TODO
+test('when a timer is showing, the skip button should move to the next scene', async () => {
+  expect(true).toBeTruthy();
+})
+
+// TODO
+test('on the end screen, the back-to-start button should move to the start screen', async () => {
+  expect(true).toBeTruthy();
+})
+
+// TODO
+test('on the end screen, the close button should close the app', async () => {
+  expect(true).toBeTruthy();
+})
